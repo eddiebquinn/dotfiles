4cc196f667124c0945f41625a0905e6463c72f42
eddiebquinn <github@pranonym.com>
2022-10-16 10:02:01 +0100

    Blind commit

--- zsh/aliases/git.zsh
+++ zsh/aliases/git.zsh
@@ -1,6 +1,7 @@
 # Basics
 alias g="git"
 alias gs="git status" 		# List changed files
+alias gl="git log"		# Displays git log
 alias ga="git add" 		# Add <files> to the next commit
 alias grm="git rm" 		# Remove <file>
 alias gc="git commit" 		# Commit staged files, needs -m ""
